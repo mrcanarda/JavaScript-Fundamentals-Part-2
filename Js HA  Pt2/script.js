8e9382194c5ab6abd5ffb263e6eb7fd649a10d74
Can Arda <canarda.dev@gmail.com>
2023-05-29 17:34:37 +0200

    Add files via upload

--- Js HA  Pt2/script.js
+++ Js HA  Pt2/script.js
@@ -443,3 +443,124 @@ scoreDolphins = calcAverage(85, 54, 41);
 scoreKoalas = calcAverage(23, 34, 27);
 console.log(scoreDolphins, scoreKoalas);
 checkWinner(scoreDolphins, scoreKoalas );
+
+
+//Introductions to Arrays 
+
+const friend1 = "Micheal"; 
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Micheal", "Steven", "Peter" ];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+//[]
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends [2] = "Jay";
+console.log(friends[2]);
+
+const firstName = "Can";
+const can = [firstName, "Arda", 2023 - 1995, "teacher", friends ];
+console.log(can);
+console.log(can.length);
+
+//Exercise 
+
+const calcAge = function (birhtYear) {
+return 2037 - birhtYear;
+}
+
+const years = [1991, 1970, 1960, 2022, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[2]);
+const age4 = calcAge(years[3]);
+const age5 = calcAge(years[4]);
+
+console.log(age1, age2, age3, age4 );
+
+const ages = [calcAge(years[0]), calcAge(years[1]),
+calcAge(years[years.length - 1])];
+
+console.log(ages);
+
+const population = [10, 1441, 332, 83];
+console.log(population.length === 4 );
+const percentages = [
+    percentageOfWorld1(population[0]),
+    percentageOfWorld1(population[1]),
+    percentageOfWorld1(population[2]),
+    percentageOfWorld1(population[3])
+];
+console.log(percentages);
+
+
+ const friends = ["Micheal", "Steven", "Peter" ];
+
+ //Add elements
+const newLength = friends.push("Jay");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("John");
+console.log(friends);
+
+//Remove elements
+
+friends.pop(); // Last 
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First 
+console.log(friends);
+
+console.log(friends.indexOf ("Steven"));
+console.log(friends.indexOf("Bob"));
+
+friends.push(23);
+console.log(friends.includes("Steven")); //Find a veriable
+console.log(friends.includes("Bob"));
+console.log(friends.includes(23)); 
+
+if (friends.includes("Steven")) {
+    console.log("You have a friend called Steven")
+} 
+
+//HA 
+
+const neighbours = ["Norway", "Sweden", "Russia"];
+
+neighbours.push("Utopa");
+console.log(neighbours);
+
+neighbours.pop();
+console.log(neighbours);
+
+if (!neighbours.includes("Italy")) {
+    console.log("Proably not a central European country :D")
+}
+
+neighbours[neighbours.indexOf("Sweden")] = "Rebublic of sweden"
+console.log(neighbours);
+
+[]
+
+
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log( bills, tips);
