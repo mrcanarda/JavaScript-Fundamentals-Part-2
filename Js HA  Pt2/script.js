70174f146c1229118a1c8e8c26cd5e3bdbb1e844
Can Arda <canarda.dev@gmail.com>
2023-06-07 00:10:25 +0200

    Add files via upload

--- Js HA  Pt2/script.js
+++ Js HA  Pt2/script.js
@@ -654,3 +654,79 @@ const friends = ["Micheal", "Steven", "Peter" ];
 console.log(friends);
 
 const y = new Array(1991, 1984, 2008, 2020);
+
+
+
+//Object Methods 
+
+const jonas ={
+    firstName: "Jonas", 
+    lastName: "Schmedtmann", 
+    birthYear: 1991, 
+    job: "teacher", 
+    friends: ["Micheal", "Peter", "Steven"], 
+    hasDriversLicense: true, 
+
+   // calcAge: function (birthYear) {
+     //  return 2037 - birthYear;
+   // }
+
+  //  calcAge: function() { 
+    //    console.log(this); 
+      //  return 2037 - this.birthYear;
+ //   }
+
+    calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+    },
+
+    getSummary: function () {
+        return `${this.firstName} is a ${this.calcAge()}-year old 
+        ${jonas.job}, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license.`}
+}; 
+
+console.log(jonas.calcAge());
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+console.log(jonas.getSummary());
+
+
+
+
+
+const mark = {
+    fullName: "Mark Miller", 
+    mass: 78, 
+    height: 1.69, 
+    calcBMI: function() {
+        this.bmi = this.mass / this.height ** 2; 
+        return this.bmi; 
+    }
+}; 
+
+const john = {
+    fullName: "John Smith", 
+    mass: 92, 
+    height: 1.95, 
+    calcBMI: function() {
+        this.bmi = this.mass / this.height ** 2; 
+        return this.bmi
+    }
+};
+
+mark.calcBMI(); 
+john.calcBMI(); 
+
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+    console.log(`${mark.fullName}'s BMI (${mark.bmi})
+    is higher than ${john.fullName}'s BMI (${john.bmi})`) 
+} else if (john.bmi > mark.bmi) {
+    console.log(`${john.fullName}'s BMI (${john.bmi}) 
+    is higher than ${mark.fullName}'s BMI (${mark.bmmi}`)
+}
