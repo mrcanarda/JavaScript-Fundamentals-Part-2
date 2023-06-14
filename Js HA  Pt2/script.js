9e9d74c48f1983b0472fd2deb6e5acdaae5acf1c
Can Arda <canarda.dev@gmail.com>
2023-06-14 02:53:51 +0200

    Add files via upload

--- Js HA  Pt2/script.js
+++ Js HA  Pt2/script.js
@@ -799,3 +799,61 @@ for(let i = 0; i < can.length; i++) {
     
     console.log(can[i], typeof can[i]);
 }
+
+const can = [
+    'Can',
+    'Arda', 
+    2037 - 1995, 
+    'developer', 
+    ["Micheal", "Peter", "Steven"], 
+    true
+]; 
+
+for(let i=can.length - 1; i>= 0; i--) {
+    console.log(i, can[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`--------------Starting exercise ${exercise}`); 
+
+    for(let rep = 1; rep <= 10; rep++) {
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+    }
+}
+
+
+let dice = Math.trunc(Math.random() * 6) + 1; 
+//console.log(dice);
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`); 
+    dice = Math.trunc(Math.random() * 6) + 1; 
+    if (dice === 6) console.log('Loop is about to end...'); 
+}
+
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 :
+    bill * 0.2; 
+}
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]; 
+const tips = []; 
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]); 
+    tips.push(tip); 
+    totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) { 
+    let sum = 0; 
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]; 
+    }
+    return sum / arr.length; 
+}
+
+console.log(calcAverage([2,3,7]));
+console.log(calcAverage(totals)); 
+console.log(calcAverage(tips)); 
